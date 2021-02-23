cc3cf1f18e26d080c392f5e3476dca8c6419ae80
Alexander Mangazeev <alex3287@bk.ru>
2021-02-23 23:22:34 +0700

    create project

---
+++ Product.h
@@ -0,0 +1,16 @@
+//
+// Created by Александр Мангазеев on 23.02.2021.
+//
+
+#ifndef SE_LABA_1_PRODUCT_H
+#define SE_LABA_1_PRODUCT_H
+
+
+
+class Product {
+
+};
+
+
+
+#endif //SE_LABA_1_PRODUCT_H
