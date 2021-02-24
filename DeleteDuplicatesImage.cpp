1b3b189f15df98e918955dcefd18954a3b660976
Преподаватель информатики кабинет №310 <kab310teacher@gym11.local>
2021-02-24 15:20:44 +0700

    create Delete DuplicatesImage

---
+++ DeleteDuplicatesImage.cpp
@@ -0,0 +1,11 @@
+//
+// Created by kab310teacher on 24.02.2021.
+//
+
+#include <iostream>  // TODO ВОПРОС это можно тут писать или лучше в hedor файл
+#include "DeleteDuplicatesImage.h"
+
+int DeleteDuplicatesImage::DeleteDuplicates() {
+    std::cout<<"Удалил 2 дубликата"<<std::endl;
+    return 2;
+}
