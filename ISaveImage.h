11d4a3bb4b3d572a8ed6cbfd190d8bc9d5220792
Преподаватель информатики кабинет №310 <kab310teacher@gym11.local>
2021-02-24 15:13:48 +0700

    create class save

---
+++ ISaveImage.h
@@ -0,0 +1,14 @@
+//
+// Created by kab310teacher on 24.02.2021.
+//
+
+#ifndef SE_LABA_1_ISAVEIMAGE_H
+#define SE_LABA_1_ISAVEIMAGE_H
+
+#include "Image.h"
+
+class ISaveImage{
+    virtual void Save(Image image)=0;
+};
+
+#endif //SE_LABA_1_ISAVEIMAGE_H
---
+++ SaveImage.h
@@ -0,0 +1,18 @@
+//
+// Created by kab310teacher on 24.02.2021.
+//
+
+#ifndef SE_LABA_1_SAVEIMAGE_H
+#define SE_LABA_1_SAVEIMAGE_H
+
+#include "ISaveImage.h"
+
+class SaveImage {
+    static void Save(Image image){
+// сохранение изображение в файловой системе
+    std::cout<<"Данные успешно сохранены в памяти компьютера"<<std::endl;
+ }
+};
+
+
+#endif //SE_LABA_1_SAVEIMAGE_H
